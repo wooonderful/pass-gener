d6beb165b55e7068e320c23914209b460681d074
wooonderful <wooonderful@outlook.com>
2022-10-14 10:40:18 +0800

    refactor: rename execute file name

--- install_all_platform.sh
+++ install_all_platform.sh
@@ -4,4 +4,4 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o pass-gener-linux  pass-gener.g
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o pass-gener-windows.exe pass-gener.go
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o pass-gener-darwin pass-gener.go
 
-CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o pass-gener_linux-arm pass-gener.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o pass-gener-linux-arm pass-gener.go
